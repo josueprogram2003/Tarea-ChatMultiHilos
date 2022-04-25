ef66244089aa31f589c0f3670c79abbd6a3476fa
josueprogram2003 <josuedominguezonline12@gmail.com>
2022-04-25 17:11:13 -0500

    tarea hecha

---
+++ src/Main/Aplicacion.java
@@ -0,0 +1,11 @@
+package Main;
+
+import views.PrincipalView;
+
+public class Aplicacion {
+	 public static void main(String[] args) {
+	      
+	        PrincipalView principal = new PrincipalView();
+	        principal.setVisible(true);
+	    }
+}
